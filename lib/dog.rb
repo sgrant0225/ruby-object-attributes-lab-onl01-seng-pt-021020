b65bfe6ee912ef5a60d67ecaebd345d6b8aec18b
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-25 01:44:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  def name=(name) 
+    @name = name
+  end
+def name
+  @name
+end
+end  
+ 
+ class Dog 
+   
+   def breed=(dogs_breed)
+     @breed = dogs_breed
+    end
+    
+    def breed
+      @breed
+    end
+  end  
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+def name=(name_of_person)
+  @name = name_of_person
+end
+
+def name 
+  @name
+ end
+end  
+
+class Person 
+
+def job=(person_job)
+  @job = person_job
+end
+
+def job
+  @job
+ end
+end  
